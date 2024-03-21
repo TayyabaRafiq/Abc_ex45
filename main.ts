c815c62b66862ed64323087d538258d1dbde709a
Tayyaba Rafique <tayyabaaliowais@gmail.com>
2024-03-21 15:41:38 -0700

    first commit

---
+++ main.ts
@@ -0,0 +1,62 @@
+//Q2
+// let personName ="Tayyaba";
+// console.log(`hello`,personName,"would you like to learn some Typescript today");
+
+//Q3
+/*let personName= "Tayyaba";
+console.log(personName.toLowerCase());
+console.log(personName.toUpperCase());
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase());*/
+//console.log("tayyaba\nTAYYABA\nTayyaba");
+
+//Famous Quote:Q4&5
+/*let Quote: string= "A person who never made a mistake never tried anything new.";
+let author:string ="Albert Einstein";
+console.log(`${author} once said, "${Quote}"`);*/
+
+/*let famousPerson: string= "Albert Einstein";
+let Quote: string ="A person who never made a mistake never tried anything new.";
+let message: string =`${famousPerson} once said, "${Quote}"`;
+console.log(message);*/
+
+//Q6 stripping:
+/*let personName: string= `\n\t Tayyaba Rafiq \t\n`;
+console.log(personName);
+
+let stripping: string= personName.trim();
+console.log(stripping);*/
+
+//Q7&8 Number eight:
+//plus:
+//console.log(5+3);
+
+//minus:
+//console.log(16-8);
+
+//Multiplication:
+//console.log(4*2);
+
+//Division:
+//console.log(16/2);
+
+//Q9 Favourite number:
+
+//let favouriteNumber: number= 5;
+//console.log(`My favourite Number is "${favouriteNumber}"`);
+
+//Q10 Adding comments:
+//Tayyaba Rafiq
+//5-3-2024
+
+//Print simple program:
+console.log(`I have done my masters in phisiology`);
+
+//print maths calculation:
+console.log(22-10+2);
+
+
+
+
+
+
+
